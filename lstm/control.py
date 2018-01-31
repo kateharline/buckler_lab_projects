d5d0e926fbd953c138ffedbde87ddb7e62c48851
Kate Harline <kharline@wustl.edu>
2018-01-31 12:56:31 -0500

    added score function that says A's predict higher expression levels

--- lstm/control.py
+++ lstm/control.py
@@ -16,10 +16,27 @@ def make_xs(n, l, t):
     return seqs
 
 
-def make_ys(seqs):
+def make_ys(seqs, rules):
     '''translate random sequences into expression values based on rules above'''
+    scores = []
 
-    return None
+    for seq in seqs:
+        for rule in rules:
+            scores.append(rule(seq))
+
+    return scores
+
+def rule_1(seq):
+    a_count = seq.count('A')
+    score = a_count*10 / len(seq)
+
+    return score
+
+def rule_2(seq):
+    a_count = seq.count('A')
+    score = a_count*2 / len(seq)
+
+    return score
 
 
 def main():
@@ -29,8 +46,11 @@ def main():
     dna_xs = make_xs(n, l, ['A', 'C', 'G', 'T'])
     protein_xs = make_xs(n, l, ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I',
                                'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'])
-    dna_ys = make_ys(dna_xs)
-    protein_ys = make_ys(protein_xs)
+    dna_ys = make_ys(dna_xs, [rule_2])
+    protein_ys = make_ys(protein_xs, [rule_1])
+
+    print('dna_ys '+str(dna_ys))
+    print('protein_ys '+str(protein_ys))
 
 if __name__ == '__main__':
     main()
